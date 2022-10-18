a7bad41c9290d7e8390ffa9c3909249778a3998b
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-18 20:07:05 +0000

    Typed navbar branding

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import './rmPortfolio.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div><h1>Roger's Blog</h1></div>>
+    <>
+    <Navbar />
+    </>
   );
 }
 
