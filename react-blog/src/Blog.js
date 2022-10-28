048209ef8d73a05938817d148d71f0fcd2ca63d1
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-10-28 13:07:56 +0000

    Installed and set up markdown-to-jsx

--- react-blog/src/Blog.js
+++ react-blog/src/Blog.js
@@ -1,9 +1,35 @@
-function Blog() {
-    return (
-        <div>
-            <h1 className="text-light">Blog Page</h1>
+import Sprint1File from "./sprint01.md"; 
+import ReactMarkdown from "react-markdown";
+import Markdown from "markdown-to-jsx";
+import { Component, useState } from "react";
+
+//const [BlogPost, setBlogPost] = useState("Sprint01"); 
+
+class Blog extends Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = { md: "" }
+    }
+  
+    componentWillMount() {
+      fetch(Sprint1File)
+        .then((res) => res.text())
+        .then((md) => {
+          this.setState({ md })
+        })
+    }
+  
+    render() {
+  
+      let { md } = this.state
+  
+      return (
+        <div className="post text-light">
+          <Markdown children={md}/>
         </div>
-    )
-};
+      )
+    }
+  }
 
 export default Blog;
